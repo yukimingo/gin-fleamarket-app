c87ad87c2b3efc95dd239f9c4bd5e395e779f5b5
yukimingo <hakamori5junk.1@gmail.com>
2024-07-15 02:32:22 +0900

    item_controller.go作成

---
+++ controllers/item_controller.go
@@ -0,0 +1,22 @@
+package controllers
+
+import (
+	"gin-fleamarket/models"
+	"gin-fleamarket/services"
+)
+
+type IItemController interface {
+	FindAll() (*[]models.Item, error)
+}
+
+type ItemController struct {
+	service services.IItemService
+}
+
+func NewItemController(service services.IItemService) IItemController {
+	return &ItemController{service: service}
+}
+
+func (c ItemController) FindAll() (*[]models.Item, error) {
+	return c.service.FindAll()
+}
