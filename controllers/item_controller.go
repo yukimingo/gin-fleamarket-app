bbb1c137de9d75a5bf696a5953327db5c561b78a
yukimingo <hakamori5junk.1@gmail.com>
2024-07-15 02:45:49 +0900

    delete comment

--- controllers/item_controller.go
+++ controllers/item_controller.go
@@ -20,5 +20,3 @@ func NewItemController(service services.IItemService) IItemController {
 func (c ItemController) FindAll() (*[]models.Item, error) {
 	return c.service.FindAll()
 }
-
-jfdks;ljfdklsa
