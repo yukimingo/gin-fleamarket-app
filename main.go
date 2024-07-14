149b07153ef08da819e7847cce968da5c76b9b5a
yukimingo <hakamori5junk.1@gmail.com>
2024-07-14 23:32:52 +0900

    initial commit

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"gin-fleamarket/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	items := []models.Item{
+		{ID: 1, Name: "商品", Price: 1000, Description: "説明1", SoldOut: false},
+		{ID: 2, Name: "商品2", Price: 2000, Description: "説明2", SoldOut: true},
+		{ID: 3, Name: "商品3", Price: 3000, Description: "説明3", SoldOut: false},
+	}
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run("localhost:8080")
+}
