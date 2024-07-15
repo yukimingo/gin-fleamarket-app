ce01b560c9a88d18323736ce8baa4ce9e9538fa1
yukimingo <hakamori5junk.1@gmail.com>
2024-07-15 15:19:18 +0900

    item_controller.go修正

--- controllers/item_controller.go
+++ controllers/item_controller.go
@@ -3,13 +3,14 @@ package controllers
 import (
 	"gin-fleamarket/services"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
 type IItemController interface {
 	FindAll(ctx *gin.Context)
-	FindById(ctx *gin.Context, itemId uint)
+	FindById(ctx *gin.Context)
 }
 
 type ItemController struct {
@@ -30,9 +31,19 @@ func (c *ItemController) FindAll(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, gin.H{"data": items})
 }
 
-func (c *ItemController) FindById(ctx *gin.Context, itemId uint) {
-	item, err := c.service.FindById(itemId)
+func (c *ItemController) FindById(ctx *gin.Context) {
+	itemId, err := strconv.ParseUint(ctx.Param("id"), 10, 64)
 	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid id"})
+		return
+	}
+
+	item, err := c.service.FindById(uint(itemId))
+	if err != nil {
+		if err.Error() == "Item not found" {
+			ctx.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+			return
+		}
 		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Unexpected error"})
 		return
 	}
