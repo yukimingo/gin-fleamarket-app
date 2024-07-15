e4fa1f468ef70682b49e58d6cd9561222fd2247d
yukimingo <hakamori5junk.1@gmail.com>
2024-07-15 12:38:34 +0900

    item_controller.go修正

--- controllers/item_controller.go
+++ controllers/item_controller.go
@@ -1,12 +1,14 @@
 package controllers
 
 import (
-	"gin-fleamarket/models"
 	"gin-fleamarket/services"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 type IItemController interface {
-	FindAll() (*[]models.Item, error)
+	FindAll(ctx *gin.Context)
 }
 
 type ItemController struct {
@@ -17,6 +19,12 @@ func NewItemController(service services.IItemService) IItemController {
 	return &ItemController{service: service}
 }
 
-func (c ItemController) FindAll() (*[]models.Item, error) {
-	return c.service.FindAll()
+func (c *ItemController) FindAll(ctx *gin.Context) {
+	items, err := c.service.FindAll()
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Unexpected error"})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"data": items})
 }
