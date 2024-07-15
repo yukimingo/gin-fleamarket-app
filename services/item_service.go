277f0764565ecd81b9bc0119f96a983eb1d2f5de
yukimingo <hakamori5junk.1@gmail.com>
2024-07-15 16:27:45 +0900

    item_service.goにcreateを追加

--- services/item_service.go
+++ services/item_service.go
@@ -1,6 +1,7 @@
 package services
 
 import (
+	"gin-fleamarket/dto"
 	"gin-fleamarket/models"
 	"gin-fleamarket/repositories"
 )
@@ -8,6 +9,7 @@ import (
 type IItemService interface {
 	FindAll() (*[]models.Item, error)
 	FindById(itemId uint) (*models.Item, error)
+	Create(createItemInput dto.CreateItemInput) (*models.Item, error)
 }
 
 type ItemService struct {
@@ -24,4 +26,14 @@ func (s *ItemService) FindAll() (*[]models.Item, error) {
 
 func (s *ItemService) FindById(itemId uint) (*models.Item, error) {
 	return s.repository.FindById(itemId)
+}
+
+func (s *ItemService) Create(createItemInput dto.CreateItemInput) (*models.Item, error) {
+	newItem := models.Item{
+		Name:        createItemInput.Name,
+		Price:       createItemInput.Price,
+		Description: createItemInput.Description,
+		SoldOut:     false,
+	}
+	return s.repository.Create(newItem)
 }
