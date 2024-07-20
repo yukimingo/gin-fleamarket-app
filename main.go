3ec3a2147e4e43a48eb4e7ebd6b1c33910539811
yukimingo <hakamori5junk.1@gmail.com>
2024-07-20 19:37:34 +0900

    signupのルーティング追加

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"gin-fleamarket/controllers"
 	"gin-fleamarket/infra"
 
-	// "gin-fleamarket/models"
 	"gin-fleamarket/repositories"
 	"gin-fleamarket/services"
 
@@ -14,23 +13,25 @@ import (
 func main() {
 	infra.Initialize()
 	db := infra.SetupDB()
-	// items := []models.Item{
-	// 	{ID: 1, Name: "商品", Price: 1000, Description: "説明1", SoldOut: false},
-	// 	{ID: 2, Name: "商品2", Price: 2000, Description: "説明2", SoldOut: true},
-	// 	{ID: 3, Name: "商品3", Price: 3000, Description: "説明3", SoldOut: false},
-	// }
 
-	// itemRepository := repositories.NewItemMemoryRepository(items)
 	itemRepository := repositories.NewItemRepository(db)
 	itemService := services.NewItemService(itemRepository)
 	itemController := controllers.NewItemController(itemService)
 
+	authRepository := repositories.NewAuthRepository(db)
+	authService := services.NewAuthService(authRepository)
+	authController := controllers.NewAuthController(authService)
+
 	r := gin.Default()
 	itemRouter := r.Group("/items")
+	authRouter := r.Group("/auth")
+
 	itemRouter.GET("", itemController.FindAll)
 	itemRouter.GET("/:id", itemController.FindById)
 	itemRouter.POST("", itemController.Create)
 	itemRouter.PUT("/:id", itemController.Update)
 	itemRouter.DELETE("/:id", itemController.Delete)
+
+	authRouter.POST("/signup", authController.Signup)
 	r.Run("localhost:8080")
 }
