a3a05fa70cfbc7161967db17b606d3a76278f2ec
yukimingo <hakamori5junk.1@gmail.com>
2024-07-20 19:59:50 +0900

    login用のdto作成

--- dto/auth_dto.go
+++ dto/auth_dto.go
@@ -4,3 +4,8 @@ type SignupInput struct {
 	Email    string `json:"email" binding:"required,email"`
 	Password string `json:"password" binding:"required,min=8"`
 }
+
+type LoginInput struct {
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min=8"`
+}
