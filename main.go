e5f101d5ec3969c1bc7659a2ac36b8cc1b08441b
yukimingo <hakamori5junk.1@gmail.com>
2024-07-20 15:48:36 +0900

    データベースのcrud処理実装

--- main.go
+++ main.go
@@ -3,25 +3,25 @@ package main
 import (
 	"gin-fleamarket/controllers"
 	"gin-fleamarket/infra"
-	"gin-fleamarket/models"
+
+	// "gin-fleamarket/models"
 	"gin-fleamarket/repositories"
 	"gin-fleamarket/services"
-	"log"
-	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	infra.Initialize()
-	log.Println(os.Getenv("ENV"))
-	items := []models.Item{
-		{ID: 1, Name: "商品", Price: 1000, Description: "説明1", SoldOut: false},
-		{ID: 2, Name: "商品2", Price: 2000, Description: "説明2", SoldOut: true},
-		{ID: 3, Name: "商品3", Price: 3000, Description: "説明3", SoldOut: false},
-	}
+	db := infra.SetupDB()
+	// items := []models.Item{
+	// 	{ID: 1, Name: "商品", Price: 1000, Description: "説明1", SoldOut: false},
+	// 	{ID: 2, Name: "商品2", Price: 2000, Description: "説明2", SoldOut: true},
+	// 	{ID: 3, Name: "商品3", Price: 3000, Description: "説明3", SoldOut: false},
+	// }
 
-	itemRepository := repositories.NewItemMemoryRepository(items)
+	// itemRepository := repositories.NewItemMemoryRepository(items)
+	itemRepository := repositories.NewItemRepository(db)
 	itemService := services.NewItemService(itemRepository)
 	itemController := controllers.NewItemController(itemService)
 
