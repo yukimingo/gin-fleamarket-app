cbca69868e6123addd7405a1009bbddd349fd987
yukimingo <hakamori5junk.1@gmail.com>
2024-07-27 12:57:10 +0900

    testのセットアップ

---
+++ main_test.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"gin-fleamarket/infra"
+	"gin-fleamarket/models"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"gorm.io/gorm"
+)
+
+func TestMain(m *testing.M) {
+	if err := godotenv.Load(".env.test"); err != nil {
+		log.Fatalln("Error loading .env.test file")
+	}
+
+	code := m.Run()
+
+	os.Exit(code)
+}
+
+func setupTestData(db *gorm.DB) {
+	items := []models.Item{
+		{Name: "テストアイテム1", Price: 1000, Description: "", SoldOut: false, UserID: 1},
+		{Name: "テストアイテム2", Price: 2000, Description: "テスト2", SoldOut: true, UserID: 1},
+		{Name: "テストアイテム3", Price: 3000, Description: "テスト3", SoldOut: false, UserID: 2},
+	}
+
+	users := []models.User{
+		{Email: "test1@example.com", Password: "test1pass"},
+		{Email: "test2@example.com", Password: "test2pass"},
+	}
+
+	for _, user := range users {
+		db.Create(&user)
+	}
+	for _, item := range items {
+		db.Create(&item)
+	}
+}
+
+func setup() *gin.Engine {
+	db := infra.SetupDB()
+	db.AutoMigrate(&models.Item{}, &models.User{})
+
+	setupTestData(db)
+	router := setupRouter(db)
+
+	return router
+}
